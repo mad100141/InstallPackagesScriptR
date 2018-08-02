faeb86daffbb06546126fa4819c513ceb676b193
Maria Rodriguez <mad100141@gmail.com>
2018-08-02 16:17:21 -0400

    "Initial"

---
+++ installPackages.R
@@ -0,0 +1,94 @@
+#install.packages("")
+
+#Basic: Data Manipulation
+install.packages("plyr") #substitutes split, apply, combine
+install.packages("dplyr")
+install.packages("tidyverse")
+install.packages("tidyr")
+install.packages("stringr")
+install.packages("reshape2")
+install.packages("rmarkdown")
+install.packages("kable")
+install.packages("Rcpp")
+install.packages("data.table")
+install.packages("devtools")
+install.packages("crayon")
+install.packages("dbplyr")
+install.packages("forcats")
+install.packages("formatR")
+install.packages("jpeg")
+install.packages("knitr")
+install.packages("pander")
+install.packages("purrr")
+install.packages("rvest")
+install.packages("Rserve")
+
+##VizViz
+install.packages("ggplot2")
+install.packages("ggmap")
+install.packages("gdata")
+install.packages("gclus")
+install.packages("ggExtra")
+install.packages("ggrepel")
+install.packages("ggsci")
+install.packages("ggsignif")
+install.packages("RColorBrewer")
+install.packages("dendextend")
+install.packages("factoextra")
+install.packages("FactoMineR")
+install.packages("igraph")
+#install.packages("vcd") #Viz for categorical data
+
+##Import
+install.packages("readr")
+# install.packages("xlsx")
+# install.packages("foreign")
+
+#Date Manipulation
+install.packages("lubridate")
+
+##Algorithms
+# install.packages("randomForest")
+# install.packages("mgcv")
+# install.packages("lme4")
+# install.packages("xgboost)
+# install.packages("nlme")
+# install.packages("multcomp")
+# install.packages("glmnet")
+# install.packages("caret")
+#install.packages("cluster")
+#install.packages("clValid")
+# install.packages("gam")
+# install.packages("gbm")
+#install.packages("np")
+
+##Association Rules
+# install.packages("arules")
+# install.packages("arulesViz")
+
+##For Parallel Work
+install.packages("doParallel")
+install.packages("foreach")
+install.packages("parallel")
+
+##For Network Visualization
+install.packages("GGally")
+install.packages("intergraph")
+install.packages("igraph")
+
+##SQL Manipulation for Data Frames
+#install.packages("sqldf") #uses sqlite syntax
+
+##Time Series Analysis
+# install.packages("forecast")
+# install.packages("dygraphs")
+
+##Financial Analysis
+#install.packages("quantmod")
+
+##Database Drivers, Run R Directly on Databases without importing to R
+# install.packages("RPostgreSQL")
+# install.packages("RMySQL")
+# install.packages("RMongo")
+# install.packages("RODBC")
+# install.packages("RSQLite")
